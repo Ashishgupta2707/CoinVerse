efaff5e5d75b816c392f5f0f6fa74e799868823c
Ashishgupta2707 <ashishguptaitself@gmail.com>
2026-02-08 12:16:12 +0530

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-600 hover:text-green-900 font-bold hover:underline cursor-pointer">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
